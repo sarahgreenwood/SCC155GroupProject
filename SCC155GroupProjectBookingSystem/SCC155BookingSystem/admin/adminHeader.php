0e2439bffe18c9149b7be14cff0b62e21b8bce3d
sarahhawksworth <sarah.hawksworth75@gmail.com>
2018-03-22 19:29:27 +0000

    Lee's web pages for the booking system

--- SCC155GroupProjectBookingSystem/SCC155BookingSystem/admin/adminHeader.php
+++ SCC155GroupProjectBookingSystem/SCC155BookingSystem/admin/adminHeader.php
@@ -8,8 +8,21 @@
         develop up to desktop devices. -->
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS which is received by the content delivery method, as opposed to downloading the files. -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- JQuery -->
+    <script type="text/javascript" src="/bower_components/jquery/dist/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    
+    <!-- Javascript and CSS for the Date picker -->
+    <script type="text/javascript" src="js/bootstrap-datetimepicker.js"></script>
+    <script type="text/javascript" src="bower_components/moment/min/moment.min.js"></script>
+    <script type="text/javascript" src="/bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+    <link rel="stylesheet" href="/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
+
+  <!-- Bootstrap CSS file -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+
 
     <!-- The title of the document, appearing within the omnibox of the browser -->
     <title>Welcome to Bridgwater and Taunton College Estates Services Dasboard</title>
--- SCC155GroupProjectBookingSystem/SCC155BookingSystem/admin/studentDashboard.php
+++ SCC155GroupProjectBookingSystem/SCC155BookingSystem/admin/studentDashboard.php
@@ -3,8 +3,16 @@
         <h1 class="display-4 text-center">Welcome to your student dashboard</h1>
 <br>
 <!-- to be displayed when there are no bookings. -->
-<p id="noBookings">You currently, don't have any booking.</p>
-
+<div class="row">
+    <div class="col-md-6">
+        <p id="noBookings">You currently, don't have any booking.</p>
+    </div>
+    <div class="col-md-6 text-right">
+        <a href="BookingForm" class="btn btn-lg btn-success">
+            <span class="glyphicon glyphicon-plus btn-lg" id="createBooking" aria-hidden="true"></span> Create a Booking
+        </a>
+    </div>
+</div>
 <br>
     <div class="row">
     <h2 class="display-6">Bookings</h2>
@@ -27,10 +35,10 @@
             <td>8/9/2919</td>
             <td>22/12/2919</td>
             <td>
-                <button type="button" class="btn btn-default" aria-label="Left Align">
+                <button type="button" class="btn btn-default" aria-label="Edit booking">
                     <span class="glyphicon glyphicon-pencil" id="editBooking" aria-hidden="true"></span>
                 </button>
-                <button type="button" class="btn btn-default" aria-label="Left Align">
+                <button type="button" class="btn btn-default" aria-label="Delete booking">
                     <span class="glyphicon glyphicon-trash" id="deleteBooking" aria-hidden="true"></span>
                 </button>
             </td>
